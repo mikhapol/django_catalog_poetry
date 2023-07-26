02b7c86c50411a745ad1a64539ea064e293de166
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-07-26 12:18:07 +0300

    Улучшение

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path
 
 from catalog_app.apps import CatalogAppConfig
-from catalog_app.views import contacts, ProductListView, CategoryListView, ProductCreateView, ProductUpdateView, \
-    ProductDetailView, ProductDeleteView, admin, VegetablesListView, vegetables
+from catalog_app.views import ProductListView, CategoryListView, ProductCreateView, ProductUpdateView, \
+    ProductDetailView, ProductDeleteView, admin, VegetablesListView, ContactTemplateView
 
 app_name = CatalogAppConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
-    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('catalog_app/categories/', CategoryListView.as_view(), name='categories'),
 
     path('catalog_app/view/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
     path('catalog_app/create/', ProductCreateView.as_view(), name='create_product'),
     path('catalog_app/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
     path('catalog_app/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
 
-    # path('/catalog_app/<int:pk>/vegetables/', VegetablesListView.as_view(), name='vegetables'),
-    path('catalog_app/<int:pk>/vegetables/', vegetables, name='vegetables'),
+    path('catalog_app/<int:pk>/vegetables/', VegetablesListView.as_view(), name='vegetables'),
+    # path('catalog_app/<int:pk>/vegetables/', vegetables, name='vegetables'),
 
-    path('contacts/', contacts, name='contacts'),
+    path('catalog_app/contacts/', ContactTemplateView.as_view(), name='contacts'),
+    # path('contacts/', contacts, name='contacts'),
     path('admin/', admin, name='admin'),
 ]
--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView, TemplateView
 
 from catalog_app.models import Product, Category
 
 
 # Create your views here.
-
-
-# def home(request):
-#     product_list = Product.objects.all()
-#     context = {
-#         'object_list': product_list,
-#         'title': 'Продукты'
-#     }
-#     return render(request, 'catalog_app/product_list.html', context)
 
 
 class ProductListView(ListView):
@@ -33,44 +24,73 @@ class CategoryListView(ListView):
 
 class VegetablesListView(ListView):
     model = Product
+    extra_context = {
+        'title': f'Категория продуктов: '
+    }
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        if category_item:
+            queryset = queryset.filter(category_id=category_item)
         return queryset
 
-    def get_context_data(self, *args, **kwargs):
-        contex_data = super().get_context_data(*args, **kwargs)
-
-        category_item = Product.objects.get(pk=self.kwargs.get('pk'))
-        contex_data['category_id'] = category_item.pk
-        contex_data['name'] = f'Продукты категории - все наши категории {category_item.name}'
-
-        return contex_data
-
-
-def vegetables(request, pk):
-    category_items = Category.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': f'Продукты {category_items.name}.'
-    }
-
-    return render(request, 'catalog_app/vegetables_list.html', context)
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+    #     return queryset
+    #
+    # def get_context_data(self, *args, **kwargs):
+    #     contex_data = super().get_context_data(*args, **kwargs)
+    #
+    #     category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+    #     contex_data['category_id'] = category_item.pk
+    #     contex_data['name'] = f'Продукты категории - все наши категории {category_item.name}'
+    #
+    #     return contex_data
 
 
-def contacts(request):
-    context = {
+# def vegetables(request, pk):
+#     category_items = Category.objects.get(pk=pk)
+#     context = {
+#         'object_list': Product.objects.filter(category_id=pk),
+#         'title': f'Продукты {category_items.name}.'
+#     }
+#
+#     return render(request, 'catalog_app/vegetables_list.html', context)
+
+
+# def contacts(request):
+#     context = {
+#         'title': 'Контакты'
+#     }
+#
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'{name} ({phone}): {message}')
+#
+#     return render(request, 'catalog_app/contacts.html', context)
+
+
+class ContactTemplateView(TemplateView):
+    template_name = 'catalog_app/contacts.html'
+    extra_context = {
         'title': 'Контакты'
     }
 
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+    def post(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
 
-    return render(request, 'catalog_app/contacts.html', context)
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'POST':
+            name = self.request.POST.get('name')
+            phone = self.request.POST.get('phone')
+            message = self.request.POST.get('message')
+            print(f'Новое сообщение от {name} ({phone}): {message}')
+        return super().get_context_data(**kwargs)
 
 
 def admin():
@@ -100,7 +120,7 @@ class ProductUpdateView(UpdateView):
 class ProductDetailView(DetailView):
     model = Product
     extra_context = {
-        'title': f'Информация о продукте.'
+        'title': 'Информация о продукте.'
     }
 
 
